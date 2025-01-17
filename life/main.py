129047d75c0ced604fa4e9b4782c43e012979e30
Jensen <jens.ta@hotmail.com>
2025-01-17 17:27:36 +0100

    performance improvements

--- life/main.py
+++ life/main.py
@@ -1,92 +1,99 @@
 import random
 import pygame
+import numpy as np
+# if numba is installed
+import numba
 
 # Constants
-WIDTH, HEIGHT = 1000, 1000
-TITLE = "Conway's game of Life"
+WIDTH : int = 1000
+HEIGHT : int = 1000
+TITLE : str = "Conway's game of Life"
 
-RESOLUTION = 20
-COLS, ROWS = WIDTH // RESOLUTION, HEIGHT // RESOLUTION
+RESOLUTION : int = 2
+
+COLS : int = WIDTH // RESOLUTION
+ROWS : int = HEIGHT // RESOLUTION
 
 # General global variable
-running = False
-framerate = 30
+running : bool = False
+framerate : int = 30
 
 # Pygame variables
-screen = None
-clock = None
+screen : None | pygame.Surface = None
+clock : None | pygame.time.Clock = None
 
 # My variables
-grid = []
+grid : np.ndarray | None = None
+
+@numba.njit(parallel=True)
+def get_random_grid(rows, cols) -> np.ndarray:
+    new = np.zeros((ROWS, COLS))
+    for i in numba.prange(rows):
+        for j in numba.prange(cols):
+            new[i, j] = random.randint(0,1)
+    return new
 
 
-def make_grid(rows, cols):
-    return [[None for j in range(cols)] for i in range(rows)]
-
-
-def setup():
+def setup() -> None:
     global screen, clock
     global running
+    global grid
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption(TITLE)
     clock = pygame.time.Clock()
+    pygame.display.set_caption(TITLE)
 
     running = True
-
-    for i in range(ROWS):
-        grid.append([])
-        for j in range(COLS):
-            grid[i].append(random.randint(0,1))
+    grid = get_random_grid(ROWS, COLS)
 
 
-def update_gui():
+def update_gui() -> None:
     screen.fill('black')
 
-    for i in range(ROWS):
-        for j in range(COLS):
-            x = i * RESOLUTION
-            y = j * RESOLUTION
-            if grid[i][j] == 1:
-                pygame.draw.rect(screen, 'white', pygame.Rect(x, y, RESOLUTION-1, RESOLUTION-1))
+    rects = [
+            pygame.Rect(i * RESOLUTION, j * RESOLUTION, RESOLUTION - 1, RESOLUTION - 1)
+            for i in range(ROWS) for j in range(COLS) if grid[i, j] == 1
+        ]
 
+    for rect in rects:
+        screen.fill('white', rect)
 
     pygame.display.flip()
     clock.tick(framerate)
 
-def update_logic():
-    global grid
+# use if numba is installed
+@numba.njit(parallel=True)
+def get_states(grid) -> np.ndarray:
+    new = np.zeros((ROWS, COLS))
 
-    new = make_grid(ROWS, COLS)
+    for i in numba.prange(1, ROWS - 1):
+        for j in numba.prange(1, COLS - 1):
+            state : int = grid[i,j]
+            neighbours : int = 0
 
-    for i in range(ROWS):
-        for j in range(COLS):
-            state = grid[i][j]
-            neighbours = 0
-
-            if (i == 0 or i == ROWS - 1) or (j == 0 or j == COLS - 1):
-                new[i][j] = state
-                continue
-
-            neighbours += grid[i-1][j-1]
-            neighbours += grid[i-1][j+1]
-            neighbours += grid[i-1][j]
-            neighbours += grid[i][j-1]
-            neighbours += grid[i][j+1]
-            neighbours += grid[i+1][j-1]
-            neighbours += grid[i+1][j+1]
-            neighbours += grid[i+1][j]
+            neighbours += grid[i-1,j-1]
+            neighbours += grid[i-1,j+1]
+            neighbours += grid[i-1,j]
+            neighbours += grid[i,j-1]
+            neighbours += grid[i,j+1]
+            neighbours += grid[i+1,j-1]
+            neighbours += grid[i+1,j+1]
+            neighbours += grid[i+1,j]
 
             if state == 0 and neighbours == 3:
-                new[i][j] = 1
+                new[i,j] = 1
             elif state == 1 and (neighbours < 2 or neighbours > 3):
-                new[i][j] = 0
+                new[i,j] = 0
             else:
-                new[i][j] = state
-    
-    grid = new
+                new[i,j] = state
+    return new
 
-def check_inputs():
+def update_logic() -> None:
+    global grid
+
+    grid = get_states(grid)
+
+def check_inputs() -> None:
     global running
 
     for event in pygame.event.get():
@@ -96,9 +103,9 @@ def check_inputs():
             if event.key in [pygame.K_ESCAPE, pygame.K_q]:
                 running = False
 
-def main():
+def main() -> None:
     setup()
-    frame = 0
+    frame : int = 0
 
     while running:
         update_logic()
